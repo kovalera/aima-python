2c52714b7d21a709e6cc1b3c5d6caec5cf951728
Kovtun Valery <kovalera@gmail.com>
2015-11-22 23:04:02 +0800

    different heuristics implemented and working, still very slow for the larger problems

--- Games_15.py
+++ Games_15.py
@@ -2,6 +2,7 @@
 Implementation of the 15 squares games by using all the different search algorithms
 learned in Chapter 3
 """
+import time
 from utils import *
 import random, copy
 import agents, search
@@ -44,9 +45,22 @@ class game_15(search.Problem):
         move[ind-self.n] = 0
         return tuple(move)
 
-    def h(self,state):
+    def h2(self, state):
         """
-        Manhattan distance heuristic
+        Inversed tiles heuristic
+        """
+        puzzle = state.state
+        count = 0
+        for i in puzzle:
+            for j in puzzle[(puzzle.index(i)+1):]:
+                if j > 0:
+                    if j < i:
+                        count = count + 1
+        return count
+
+    def h3(self,state):
+        """
+        Manhattan distance
         """
         s = 0
         r = list(state.state)
@@ -55,6 +69,43 @@ class game_15(search.Problem):
             s = s + abs(r.index(i)%self.n - g.index(i)%self.n)
             s = s + abs(r.index(i)/self.n - g.index(i)/self.n)
         return s
+
+    def h4(self,state):
+        """
+        Manhattan + inversed distance heuristic
+        so far the fastest heuristic but not the most efficient solution found
+        """
+        s = 0
+        r = list(state.state)
+        g = list(self.goal)
+        for i in r:
+            s = s + abs(r.index(i)%self.n - g.index(i)%self.n)
+            s = s + abs(r.index(i)/self.n - g.index(i)/self.n)
+        puzzle = state.state
+        count = 0
+        for i in puzzle:
+            for j in puzzle[(puzzle.index(i)+1):]:
+                if j > 0:
+                    if j < i:
+                        count = count + 1
+        return s + count
+    def h(self,state):
+        """
+        Nilsson's Sequence Score - my understanding of it -
+        speed is roughly the same as the manhattan distance
+        """
+        s = 0
+        t = 0
+        r = list(state.state)
+        g = list(self.goal)
+        for i in r:
+            s = s + abs(r.index(i)%self.n - g.index(i)%self.n)
+            s = s + abs(r.index(i)/self.n - g.index(i)/self.n)
+        for k in r:
+            if (r.index(k) != 15):
+                if (r[r.index(k)+1] != k + 1 ):
+                    t = t + 2
+        return (s + 3*t)
 
     def h1(self,state):
         """
@@ -127,6 +178,30 @@ if __name__ == "__main__":
     9,5,2,4,\
     0,7,15,8,\
     13,14,12,11 - depth 20 works with A* search
+    Puzzle_8 = [8,1,3,\
+                4,0,2,\
+                7,6,5]
+    """
+    Puzzle_8 = [1,2,3,\
+                4,5,6,\
+                0,7,8]
+    # Depth 29 - right now solved in 143 seconds
+    # With mixed heuristic solved in 9 seconds depth 35
+
+    Puzzle_15 = [1,0,10,6,\
+                 5,7,3,4,\
+                 9,15,2,11,\
+                 13,14,8,12]
+    """
+    Puzzle_15 = [5,1,10,6,\
+                 9,7,3,4,\
+                 15,2,0,11,\
+                 13,14,8,12]
+
+    Puzzle_15 = [2,7,11,5,\
+                 13,0,9,4,\
+                 14,1,8,6,\
+                 10,3,12,15]
     """
     # The following 4 algorithms used are uninformed search algorithms
     #Works - long time, long solution - depth 12
@@ -152,7 +227,53 @@ if __name__ == "__main__":
                                      12,14,15,1],4))
     print a.solution()
     print a.depth
+
+    if check_solvable(Puzzle_15,4):
+        t = search.astar_search(game_15(Puzzle_15,4))
+    else:
+        print "Puzzle is unsolvable, please change"
+
     """
-    t = check_solvable([3,1,5,0,6,8,4,2,7],3)
-    print t
-    
+    if check_solvable(Puzzle_15,4):
+        p = search.InstrumentedProblem(game_15(Puzzle_15,4))
+
+        #t = search.astar_search(p)
+        time1 = time.time()
+        t = search.astar_search(p)
+        #t = search.recursive_best_first_search(p)
+        time2 = time.time()
+        print p
+        #search.compare_searchers(problems = [game_15(Puzzle_8,3)],header=['Searcher', 'Puzzle 8'])
+        print t.solution()
+        print t.depth
+        print "time taken is:"
+        print time2 - time1
+    else:
+        print "Sorry the puzzle is not feasible"
+    """
+    count = 0
+    # Count all the inversions in the puzzle
+    puzzle = [15,14,13,12,\
+              11,10,9,8,\
+              7,6,5,4,\
+              3,2,1,0]
+    goal = [1,2,3,4,\
+            5,6,7,8,\
+            9,10,11,12,\
+            13,14,15,0]
+    for i in puzzle:
+            for j in puzzle[(puzzle.index(i)+1):]:
+                if j > 0:
+                    if j < i:
+                        count = count + 1
+
+    s = 0
+    r = puzzle
+    g = goal
+    for i in r:
+        s = s + abs(r.index(i)%4 - g.index(i)%4)
+        s = s + abs(r.index(i)/4 - g.index(i)/4)
+    print s
+
+    print count
+    """
