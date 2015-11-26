bdb4f6dc98abefc639ff5d3444d6105dc0c199c3
Kovtun Valery <kovalera@gmail.com>
2015-11-26 13:35:07 +0800

    Added the update to the Games_15 file with changed heuristic - ID

--- Games_15.py
+++ Games_15.py
@@ -58,7 +58,7 @@ class game_15(search.Problem):
                         count = count + 1
         return count
 
-    def h3(self,state):
+    def h4(self,state):
         """
         Manhattan distance
         """
@@ -70,9 +70,9 @@ class game_15(search.Problem):
             s = s + abs(r.index(i)/self.n - g.index(i)/self.n)
         return s
 
-    def h4(self,state):
+    def h(self,state):
         """
-        Manhattan + inversed distance heuristic
+        Manhattan + inverse distance heuristic
         so far the fastest heuristic but not the most efficient solution found
         """
         s = 0
@@ -82,14 +82,73 @@ class game_15(search.Problem):
             s = s + abs(r.index(i)%self.n - g.index(i)%self.n)
             s = s + abs(r.index(i)/self.n - g.index(i)/self.n)
         puzzle = state.state
-        count = 0
+        count_v = 0
+        count_h = 0
         for i in puzzle:
             for j in puzzle[(puzzle.index(i)+1):]:
                 if j > 0:
                     if j < i:
-                        count = count + 1
-        return s + count
-    def h(self,state):
+                        count_v = count_v + 1
+
+        for j in range(self.n):
+            l = self.n * j
+            for i in range(self.n):
+                k = i + l
+                res = k
+                if res + self.n == self.n**2 - 1:
+                    res = self.n**2 - 1
+                    if puzzle[k] > puzzle[res]:
+                        count_h = count_h + 1
+                else:
+                    res = (res + self.n)  % (self.n**2 - 1)
+                while res != self.n**2 - 1:
+                    if puzzle[res] > 0:
+                        if puzzle[k] > puzzle[res]:
+                            count_h = count_h + 1
+                    if (res + self.n) == self.n**2 - 1:
+                        res = self.n**2 - 1
+                        if puzzle[k] > puzzle[res]:
+                            count_h = count_h + 1
+                    else:
+                        res = (res + self.n)  % (self.n**2 - 1)
+        count_h = count_h%self.n + count_h/self.n
+        count_v = count_v%self.n + count_v/self.n
+
+        ID = count_h + count_v
+
+        return s+ID
+
+    def h1(self,state):
+        count_h = 0
+        count_v = 0
+        puzzle = state.state
+        for j in range(self.n):
+            l = self.n * j
+            for i in range(self.n):
+                k = i + l
+                res = k
+                if res + self.n == self.n**2 - 1:
+                    res = self.n**2 - 1
+                    if puzzle[k] > puzzle[res]:
+                        count_h = count_h + 1
+                else:
+                    res = (res + self.n)  % (self.n**2 - 1)
+                while res != self.n**2 - 1:
+                    if puzzle[res] > 0:
+                        if puzzle[k] > puzzle[res]:
+                            count_h = count_h + 1
+                    if (res + self.n) == self.n**2 - 1:
+                        res = self.n**2 - 1
+                        if puzzle[k] > puzzle[res]:
+                            count_h = count_h + 1
+                    else:
+                        res = (res + self.n)  % (self.n**2 - 1)
+        count_h = count_h%self.n + count_h/self.n
+        count_v = count_v%self.n + count_v/self.n
+
+        ID = count_h + count_v
+        return ID
+    def h4(self,state):
         """
         Nilsson's Sequence Score - my understanding of it -
         speed is roughly the same as the manhattan distance
@@ -107,7 +166,7 @@ class game_15(search.Problem):
                     t = t + 2
         return (s + 3*t)
 
-    def h1(self,state):
+    def h6(self,state):
         """
         Displaced tiles heuristic
         """
@@ -142,8 +201,6 @@ class game_15(search.Problem):
         return acts
 
     def result(self,state,action):
-        #state = list(action)
-        #print state
         return action
 
 def check_solvable(puzzle,n):
@@ -182,27 +239,31 @@ if __name__ == "__main__":
                 4,0,2,\
                 7,6,5]
     """
-    Puzzle_8 = [1,2,3,\
-                4,5,6,\
-                0,7,8]
+    Puzzle_8 = [4,5,8,\
+                6,7,2,\
+                0,1,3]
     # Depth 29 - right now solved in 143 seconds
-    # With mixed heuristic solved in 9 seconds depth 35
-
-    Puzzle_15 = [1,0,10,6,\
-                 5,7,3,4,\
-                 9,15,2,11,\
-                 13,14,8,12]
+    # With mixed heuristic solved in 9 seconds depth 35, 13 sec 27 depth
     """
+    Puzzle_15 = [1,7,10,6,\
+                 9,3,4,0,\
+                 15,5,2,11,\
+                 13,14,8,12]
+
     Puzzle_15 = [5,1,10,6,\
                  9,7,3,4,\
                  15,2,0,11,\
                  13,14,8,12]
-
+    """
+    Puzzle_15 = [5,3,8,11,\
+                 2,10,4,12,\
+                 13,1,9,0,\
+                 14,15,6,7]
+    """
     Puzzle_15 = [2,7,11,5,\
                  13,0,9,4,\
                  14,1,8,6,\
                  10,3,12,15]
-    """
     # The following 4 algorithms used are uninformed search algorithms
     #Works - long time, long solution - depth 12
     #a = search.uniform_cost_search(game_15([8,1,3,4,0,2,7,6,5],3))
@@ -220,7 +281,6 @@ if __name__ == "__main__":
     #                                 5,7,2,4,\
     #                                 9,15,8,11,\
     #                                 13,14,0,12],4))
-    """
     a = search.astar_search(game_15([9,5,2,4,\
                                      7,0,3,8,\
                                      10,6,13,11,\
@@ -234,8 +294,8 @@ if __name__ == "__main__":
         print "Puzzle is unsolvable, please change"
 
     """
-    if check_solvable(Puzzle_15,4):
-        p = search.InstrumentedProblem(game_15(Puzzle_15,4))
+    if check_solvable(Puzzle_8,3):
+        p = search.InstrumentedProblem(game_15(Puzzle_8,3))
 
         #t = search.astar_search(p)
         time1 = time.time()
